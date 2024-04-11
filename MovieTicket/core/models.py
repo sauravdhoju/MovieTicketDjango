0443a8ce8e76ea4a332b77c7da02f299df5ec706
Sreinumder <rojgosai02@gmail.com>
2024-04-11 19:00:55 +0545

    irreleant changes lol

--- MovieTicket/core/models.py
+++ MovieTicket/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Movie(models.Model):
+    def __str__(self):
+        return self.name+' at '+ str(self.event_date )
+    # user        = models.ForeignKey('auth.User', on_delete = models.CASCADE)
+    name  = models.CharField(max_length=80)
+    # event_date  = models.DateTimeField()
+    description = models.TextField()
+    added_date  = models.DateTimeField()
+
+class MovieSchedule(models.Model):
+    def __str__(self):
+        return self.name+' at '+ str(self.schedule_date )
+    Movie = models.ForeignKey('.models.Movie', on_delete = models.CASCADE)
+    schedule_date= models.DateTimeField()
