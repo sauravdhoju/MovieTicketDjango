7200a04d42c54e19497a0931fadc69caf0cda484
Sreinumder <rojgosai02@gmail.com>
2024-04-11 22:39:11 +0545

    more models ig

--- MovieTicket/core/models.py
+++ MovieTicket/core/models.py
@@ -1,14 +1,17 @@
-from django.db import models
+fro django.db import models
 from datetime import datetime
 
 class Movie(models.Model):
     def __str__(self):
         return self.name+' at '+ str(self.event_date )
     # user        = models.ForeignKey('auth.User', on_delete = models.CASCADE)
-    name  = models.CharField(max_length=80)
+    name  = models.CharField(max_length=255)
+
     # event_date  = models.DateTimeField()
     description = models.TextField()
     added_date  = models.DateTimeField()
+    average_rating = models.FloatField(default=0)  # Average rating
+    total_reviews = models.IntegerField(default=0) 
 
 class MovieSchedule(models.Model):
     def __str__(self):
