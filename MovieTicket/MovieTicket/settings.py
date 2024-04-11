630a7feba498d2bfd68852d430e8fd3221e9e5b2
Sreinumder <rojgosai02@gmail.com>
2024-04-11 22:38:08 +0545

    sqlite => postgresql

--- MovieTicket/MovieTicket/settings.py
+++ MovieTicket/MovieTicket/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = "MovieTicket.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'database.db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',   # or your PostgreSQL server host
+        'PORT': '5432',        # or your PostgreSQL server port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
