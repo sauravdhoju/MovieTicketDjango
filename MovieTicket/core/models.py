9c4dfd573f7eb069d9775842d14b8159a51689d4
Sreinumder <rojgosai02@gmail.com>
2024-04-12 11:56:57 +0545

    added all the models

--- MovieTicket/core/models.py
+++ MovieTicket/core/models.py
@@ -1,4 +1,4 @@
-fro django.db import models
+from django.db import models
 from datetime import datetime
 
 class Movie(models.Model):
@@ -18,3 +18,44 @@ class MovieSchedule(models.Model):
         return self.name+' at '+ str(self.schedule_date )
     Movie = models.ForeignKey('.models.Movie', on_delete = models.CASCADE)
     schedule_date= models.DateTimeField()
+
+class MovieToGenre(models.Model):
+    Movie_id = models.ForeignKey('Movie.id', on_delete = models.CASCADE)
+    Genre_id = models.ForeignKey('Genre.id', on_delete = models.CASCADE)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=40)
+
+class Hall(models.Seat):
+    title = models.CharField(max_length=40)
+    location = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
+    seats_count = models.PositiveIntegerField()
+
+class MovieUserRating(models.Model):
+    user     = models.ForeignKey('auth.User', on_delete = models.CASCADE)
+    rating = models.FloatField(default=0)  # Average rating
+
+class Comment:
+    Movie_id = models.ForeignKey('Movie.id', on_delete = models.CASCADE)
+    user     = models.ForeignKey('auth.User', on_delete = models.CASCADE)
+    comment = models.Textfield()
+    added_date = models.DateTimeField()
+    likes = models.PositiveIntegerField()
+    dislikes = models.PositiveIntegerField()
+
+class Reply:
+    reply_to_a_comment = models.BoolanField(default=True) '''if to reply false'''
+    parentComment_id = models.PositiveIntegerField()
+    user     = models.ForeignKey('auth.User', on_delete = models.CASCADE)
+    comment = models.Textfield()
+    added_date = models.DateTimeField()
+    likes = models.PositiveIntegerField()
+    dislikes = models.PositiveIntegerField()
+
+class Ticket:
+    user     = models.ForeignKey('auth.User', on_delete = models.CASCADE)
+    ticket_code = models.CharField(max_length=255)
+    seat        = models.ForeignKey('Seat.id', on_delete = models.CASCADE)
+    movie_schedule_id = models.ForeignKey('Movie.id', on_delete = models.CASCADE)
+    creation_date = models.DateTimeField()
