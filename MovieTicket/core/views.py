641a2021e260a736b342ecc360cc651b9eff6792
Sreinumder <rojgosai02@gmail.com>
2024-04-13 13:47:48 +0545

    created index view

--- MovieTicket/core/views.py
+++ MovieTicket/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from django.render import render, redirect
 # Create your views here.
+def index(request):
+    return render(request, 'home.html')
+
