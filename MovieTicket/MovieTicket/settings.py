1765aec6064506eb10ee7cb65ecd00d0d9a469f3
sauravdhoju <sauravdhoju12@gmail.com>
2024-04-14 16:28:21 +0545

    fixed for statics

--- MovieTicket/MovieTicket/settings.py
+++ MovieTicket/MovieTicket/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,8 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = [BASE_DIR /'static']
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    # [BASE_DIR /'static']
+    os.path.join(BASE_DIR, 'static/image'),
+]
+STATIC_ROOT = BASE_DIR /'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
