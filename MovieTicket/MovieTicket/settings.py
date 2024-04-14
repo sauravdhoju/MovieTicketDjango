48b8e375eb32abd8ab0e14ffec95527a9fa1dd83
sauravdhoju <sauravdhoju12@gmail.com>
2024-04-14 15:06:39 +0545

    static_url dir template dir fixed and

--- MovieTicket/MovieTicket/settings.py
+++ MovieTicket/MovieTicket/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "MovieTicket.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -76,13 +76,15 @@ WSGI_APPLICATION = "MovieTicket.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'database.db',
+        'NAME': 'master',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',   # or your PostgreSQL server host
         'PORT': '5432',        # or your PostgreSQL server port
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -118,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = [BASE_DIR /'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
