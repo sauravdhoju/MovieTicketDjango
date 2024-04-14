3ccd9165d54176f8120e7ac4288fb784968f4d85
sauravdhoju <sauravdhoju12@gmail.com>
2024-04-14 15:07:45 +0545

    fixed render func

--- MovieTicket/core/views.py
+++ MovieTicket/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
-from django.render import render, redirect
+# from django.render import render redirect/
 # Create your views here.
 def index(request):
     return render(request, 'home.html')
