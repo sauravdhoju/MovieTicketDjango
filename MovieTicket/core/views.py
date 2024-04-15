f3ef119aa75ac84859b29f0b78c07769b6395e94
Sreinumder <rojgosai02@gmail.com>
2024-04-15 14:08:55 +0545

    login_signup

--- MovieTicket/core/views.py
+++ MovieTicket/core/views.py
@@ -11,26 +11,26 @@ from core.models import MovieSchedule
 def index(request):
     return render(request, 'home.html')
 
-def login_signup(request):
-    # return render(request, 'login_signup.html') 
-    if request.method == 'POST':
-        if 'login' in request.POST:
-            username=request.POST.get('username')
-            passsword=request.POST.get('password')  
-            user=authenticate(request, username=username,password=passsword)
-            if user is not None:
-                login(request,user)
-                return redirect("calculate")
-
-        elif 'signup' in request.POST:
-            username = request.POST.get('Username')
-            password = request.POST.get('Password')
-            email    = request.POST.get('Email')
-    else:
-        return render(request, 'login_signup.html') 
-    #         my_user = User.objects.create_user(username,email,password1)
-    #         my_user.save()
-    #         # return redirect('login') 
+# def login_signup(request):
+#     # return render(request, 'login_signup.html') 
+#     if request.method == 'POST':
+#         if 'login' in request.POST:
+#             username=request.POST.get('username')
+#             passsword=request.POST.get('password')  
+#             user=authenticate(request, username=username,password=passsword)
+#             if user is not None:
+#                 login(request,user)
+#                 return redirect("calculate")
+#
+#         elif 'signup' in request.POST:
+#             username = request.POST.get('Username')
+#             password = request.POST.get('Password')
+#             email    = request.POST.get('Email')
+#     else:
+#         return render(request, 'login_signup.html') 
+#     #         my_user = User.objects.create_user(username,email,password1)
+#     #         my_user.save()
+#     #         # return redirect('login') 
     # if request.method=='POST':
     #
     # return render(request, 'login.html')
@@ -53,3 +53,34 @@ def login_signup(request):
     #
     #
     #     return render(request, 'movie.html', )
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 != password2:
+            return HttpResponse("Your password and confirm password are not same.")
+        else:
+            my_user = User.objects.create_user(username,email,password1)
+            my_user.save()
+            return redirect('login') 
+    return render(request, 'signup.html')    
+
+def log_in(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        passsword=request.POST.get('password')  
+        user=authenticate(request, username=username,password=passsword)
+        if user is not None:
+            login(request,user)
+            return redirect("home")
+        else:
+            return redirect('signup') 
+    return render(request, 'login.html')
+
+def log_out(request):
+    logout(request)
+    return redirect('login')
