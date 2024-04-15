353039e18e09faab64524c27f3ea3e5911b1ba66
Sreinumder <rojgosai02@gmail.com>
2024-04-15 09:43:10 +0545

    semi fixed login_signup view

--- MovieTicket/core/views.py
+++ MovieTicket/core/views.py
@@ -3,37 +3,42 @@ from django.utils import timezone
 from django.contrib.auth import login, authenticate, logout, get_user_model
 from django.contrib.auth.models import User 
 from django.contrib.auth.decorators import login_required
+from .models import Movie, 
 
 
 # from django.render import render redirect/
 # Create your views here.
 def index(request):
-    
     return render(request, 'home.html')
 
 def login_signup(request):
-    return render(request, 'login_signup.html') 
+    # return render(request, 'login_signup.html') 
     if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password1 = request.POST.get('password1')
-        password2 = request.POST.get('password2')
-        if password1 != password2:
-            return HttpResponse("Your password and confirm password are not same.")
-        else:
-            my_user = User.objects.create_user(username,email,password1)
-            my_user.save()
-            # return redirect('login') 
-    return render(request, 'login_signup.html') 
-    if request.method=='POST':
-        username=request.POST.get('username')
-        passsword=request.POST.get('password')  
-        user=authenticate(request, username=username,password=passsword)
-        if user is not None:
-            login(request,user)
-            bmi_user=user.username
-            return redirect("calculate")
-        else:
-            return redirect('signup') 
+        if 'login' in request.POST:
+            username=request.POST.get('username')
+            passsword=request.POST.get('password')  
+            user=authenticate(request, username=username,password=passsword)
+            if user is not None:
+                login(request,user)
+                return redirect("calculate")
 
-    return render(request, 'login.html')
+        elif 'signup' in request.POST:
+            username = request.POST.get('Username')
+            password = request.POST.get('Password')
+            email    = request.POST.get('Email')
+    else:
+        return render(request, 'login_signup.html') 
+    #         my_user = User.objects.create_user(username,email,password1)
+    #         my_user.save()
+    #         # return redirect('login') 
+    # if request.method=='POST':
+    #
+    # return render(request, 'login.html')
+
+@login_required(login_url = 'loginSignup')
+    def movie(request, movie_id):
+        try:
+            event = Movie.objects.get(pk=movie_id)  # pk stands for primary key
+        except Event.DoesNotExist:
+            # Handle the case where the event with the given ID doesn't exist
+            return render(request, 'event_not_found.html')
