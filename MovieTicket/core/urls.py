16438c3ca7499606c91685997dcd11b105416a56
Sreinumder <rojgosai02@gmail.com>
2024-04-16 00:42:02 +0545

    url for homepage and addMovies

--- MovieTicket/core/urls.py
+++ MovieTicket/core/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.home, name="home"),
     path('signup/', views.sign_up, name = 'signup'),
     path('login/',  views.log_in, name = 'login'),
     path('logout/', views.log_out, name='logout'),
+    path('add/', views.addMovie, name='addmovie'),
     # path("movie/<int>", views.login_signup, name="loginSignup"),
 ]
