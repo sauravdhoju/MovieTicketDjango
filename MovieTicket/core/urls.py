47a22d1eb810fef4f01c7d5a08cc34ab54211b14
Sreinumder <rojgosai02@gmail.com>
2024-04-16 14:31:53 +0545

    added booking url

--- MovieTicket/core/urls.py
+++ MovieTicket/core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/',  views.log_in, name = 'login'),
     path('logout/', views.log_out, name='logout'),
     path('movie/<int:movie_id>/', views.moviePage, name='moviepage'),
+    path('movie/<int:movie_id>/booking/', views.bookMoviePage, name='book'),
 ]
